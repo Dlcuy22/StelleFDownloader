e4c5018a8f602767a43243b6884ba404f16636ad
Asrian Putra <alifqwik@gmail.com>
2025-04-08 05:15:57 +0700

    early webui development

--- frontend/webui/app.vue
+++ frontend/webui/app.vue
@@ -1,6 +1,31 @@
+<!-- App.vue -->
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+  <div id="app">
+    <Main />
   </div>
 </template>
+
+<script>
+import Main from './components/Main.vue'; // Import the component
+
+export default {
+  name: 'App',
+  components: {
+    Main, // Register the component
+  },
+};
+</script>
+
+<style>
+@import url("https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css");
+
+html, body, #app {
+  margin: 0;
+  padding: 0;
+  margin: 0;
+  padding: 0;
+  height: 100%;
+  background-color: #1a1a1a; 
+  overflow: hidden;
+}
+</style>
